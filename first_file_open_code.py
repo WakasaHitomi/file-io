6e783c71d0bae5b2c9e431fdccca3950462fc665
WakasaHitomi <31477539+WakasaHitomi@users.noreply.github.com>
2017-12-01 09:33:23 -0500

    Add files via upload

--- first_file_open_code.py
+++ first_file_open_code.py
@@ -1,4 +1,4 @@
-with open('scrabble_list.txt', 'r') as f:
+with open('words_alpha.txt', 'r') as f:
     words = f.read().splitlines()
     
     word_count = 0
@@ -6,7 +6,8 @@ with open('scrabble_list.txt', 'r') as f:
         word_count += 1
     print(word_count)
     print("this is the number of words")
-#84009 words
+    print()
+# 370099 words
 
     five_letters = 0
     for w in words:
@@ -14,4 +15,33 @@ with open('scrabble_list.txt', 'r') as f:
             five_letters += 1
     print(five_letters)
     print("Words with only 5 letters")
-#8939
+    print()
+# 15918 five letter words
+
+    over_seven_letters = 0
+    for w in words:
+        if len(w) > 7:
+            over_seven_letters += 1
+    print(over_seven_letters)
+    print(" words that have more 7 letters")
+    print()
+# 272543 more than 7 letters
+
+    total_letters = 0
+    for w in words:
+        for letter in w:
+            total_letters += 1
+    print(total_letters)
+    print("Number of characters.")
+    print()
+# 3494678 total characters
+
+    words_without_e = 0
+    for letters in words:
+        if letter != "e":
+            words_without_e += 1
+    print(words_without_e)
+    print("words witout the letter e")
+    print()
+# 
+
